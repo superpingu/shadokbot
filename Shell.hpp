a53b6e2aa6e35844dbba946d499b7865bcb9d154
superpingu <arnaudbonetti@me.com>
2018-09-23 13:13:27 +0200

    added interactive shell to control the robot

---
+++ Shell.hpp
@@ -0,0 +1,89 @@
+#ifndef SHELL_HPP
+#define SHELL_HPP
+
+#include <stdint.h>
+
+#define SHELL_BUFFER_SIZE 20
+#define MAX_ARG_NUMBER 5
+#define SHELL_INVITE " > "
+
+struct command_t {
+	const char* name;
+	void (*callback)(int, char**);
+};
+
+int str2int(char* str) {
+	uint8_t i = 0;
+	int result = 0;
+	if(str[0] == '-' && str[1] >= '0' && str[1] <= '9') {
+		result = -(str[1]-'0');
+		i = 2;
+	}
+	while(str[i] != 0) {
+		if(str[i] >= '0' && str[i] <= '9')
+			result = result*10 + str[i] - '0';
+		else
+			return 0;
+		i++;
+	}
+	return result;
+}
+
+class Shell {
+private:
+	const command_t* commands;
+	char buffer[SHELL_BUFFER_SIZE];
+	uint8_t index;
+	char* argv[MAX_ARG_NUMBER];
+	uint8_t argc = 0;
+
+	void processCommand() {
+		uint8_t i = 0, j;
+		while(commands[i].name != NULL) {
+			j = 0;
+			while(commands[i].name[j] == buffer[j]) {
+				if(buffer[j] == 0) {
+					commands[i].callback(argc, argv);
+					return;
+				}
+				j++;
+			}
+			i++;
+		}
+		Serial.print("Unknown command\n");
+	}
+public:
+	Shell(uint32_t baudrate, const command_t* _commands) {
+		index = 0;
+		commands = _commands;
+		Serial.begin(baudrate);
+		Serial.print(SHELL_INVITE);
+	}
+
+	void update() {
+		char newchar;
+
+		while(Serial.available() > 0) {
+			newchar = Serial.read();
+			Serial.print(newchar);
+
+			if(newchar == '\n') {
+				buffer[index] = 0;
+				processCommand();
+				index = 0;
+				argc = 0;
+				Serial.print(SHELL_INVITE);
+			} else if(newchar == ' ') {
+				buffer[index++] = 0;
+			} else if(newchar != '\r') {
+				if(index == 0 || buffer[index-1] == 0) {
+					argv[argc] = &buffer[index];
+					argc++;
+				}
+				buffer[index++] = newchar;
+			}
+		}
+	}
+};
+
+#endif
