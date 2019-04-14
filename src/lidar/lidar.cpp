6442bb04b66118ed3654394108239ea9a42b4e7c
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.org>
2019-04-14 16:25:40 +0200

    Addition of read state machine

--- src/lidar/lidar.cpp
+++ src/lidar/lidar.cpp
@@ -2,6 +2,7 @@
 
 Lidar::Lidar() :
     buffer(200) {
+    stage = IDLE;
 }
 
 Lidar::~Lidar() {
@@ -9,5 +10,33 @@ Lidar::~Lidar() {
 }
 
 void Lidar::pushSampleData(uint8_t data) {
-
+    buffer.write(data);
+    switch (stage) {
+    case IDLE:
+        if (data == 0xAA)
+            stage = HEADER_START;
+        break;
+    case HEADER_START:
+        if (data == 0x55)
+            stage = HEADER_END;
+        else
+            stage = IDLE;
+        break;
+    case HEADER_END:
+        if (data == 0x00 || data == 0x01)
+            stage = CT;
+        else
+            stage = IDLE;
+        break;
+    case CT:
+        content_length = 2 * data + 4; // 2 bytes per sample + 4 bytes for angles
+        stage = LENGTH;
+        break;
+    case LENGTH:
+        content_length--;
+        if (content_length == 0) {
+            stage = IDLE;
+            parseFrame();
+        }
+    }
 }
--- src/lidar/lidar.hpp
+++ src/lidar/lidar.hpp
@@ -4,6 +4,14 @@
 #include <stdint.h>
 #include "circ_buffer.hpp"
 
+typedef enum Parsing_Stage_s {
+    IDLE,
+    HEADER_START,
+    HEADER_END,
+    CT,
+    LENGTH
+} Parsing_Stage_t;
+
 class Lidar {
 public:
     Lidar();
@@ -12,8 +20,11 @@ public:
 
 private:
     CircBuffer buffer;
+    int content_length;
+    Parsing_Stage_t stage;
 
     void parseFrame();
+
 };
 
 #endif // LIDAR_H
